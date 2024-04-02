df67097056bff56ac00d2089db750501b44d1e8a
Joydip <joydip.gope02@gmail.com>
2024-04-02 09:28:22 +0600

    Complate react project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose your ultimate destination for culinary inspiration and gastronomic adventures! Dive into a world where flavors dance on your palate and each bite tells a story. Our passion for food knows no bounds, and we are thrilled to share our culinary journey with you.</p>
+        <button>view Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
